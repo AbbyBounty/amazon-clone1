030a729b4e0c84ed6c3f721d972110b07141430e
abhilash <abhilash.kamble376@gmail.com>
2020-10-12 09:52:48 +0530

    uuid

---
+++ test-bed.js
@@ -0,0 +1,14 @@
+const uuid=require('uuid')
+
+
+const token1=uuid.v1()
+// const token3=uuid.v3()
+const token4=uuid.v4()
+// const token5=uuid.v5()
+
+
+console.log(token1)
+// console.log(token3)
+console.log(token4)
+// console.log(token5)
+
